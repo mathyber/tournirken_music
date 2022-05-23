d74f8b5e3635ffd7ce8be4acf126877a0fe8b9e5
mathyber <ilan23f@gmail.com>
2022-05-24 00:44:58 +0300

    swagger new

--- backend/app.js
+++ backend/app.js
@@ -1,53 +1,13 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
+const http = require('http');
+const swaggerFile = require('./swagger_output.json')
 
 const app = express();
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Tournirken Music API',
-            version: '1.0.0',
-            description: '---'
-        }
-    },
-    apis: ['app.js'],
-};
+http.createServer(app).listen(3000)
+console.log("Listening at:// port:%s (HTTP)", 3000)
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
-/**
- * @swagger
- * /ping:
- *  get:
- *    description: Ping
- *    responses:
- *      200:
- *        description: Success
- */
-app.get('/ping', (req, res) => {
-    res.send('pong.')
-});
-
-/**
- * @swagger
- * /test:
- *  post:
- *    description: Create test
- *    parameters:
- *    - name: title
- *      description: test title
- *      in: formData
- *      required: true
- *      type: string
- *    responses:
- *      201:
- *        description: Created
- */
-app.post('/test', (req, res) => {
-    res.status(201).send('pong.');
-});
-
-app.listen(5000, () => console.log('port 5000'))
+require('./endpoints')(app);
