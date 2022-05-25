04937ad77f12fc5e1093134f2f64c2121537be23
mathyber <ilan23f@gmail.com>
2022-05-25 22:11:12 +0300

    старт сервера, подключение к БД

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+
+const PORT = process.env.PORT || 5010;
+
+const app = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
