8b9cbbe2743fb57ac64a9b62f42a71cdb428bec9
mathyber <ilan23f@gmail.com>
2022-05-27 03:14:22 +0300

    ссылка на вход в шапке

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,9 +2,10 @@ import React, {useEffect, useState} from 'react';
 import './styles.scss';
 import {Button, Icon, Menu} from "semantic-ui-react";
 import { useLocation } from "react-router-dom";
-import {HOME_LINK} from "../../router/links";
+import {HOME_LINK, LOGIN_LINK} from "../../router/links";
 import {useDispatch, useSelector} from "react-redux";
 import {isAuthSelector, logout, profile, profileSelector} from "../../ducks/user";
+import {useNavigate} from "react-router";
 
 const Header = () => {
 
@@ -14,6 +15,8 @@ const Header = () => {
     const [activeItem, setActiveItem] = useState('');
     const isAuth = useSelector(isAuthSelector);
     const profileData = useSelector(profileSelector);
+
+    let navigate = useNavigate();
 
     useEffect(() => {
         setActiveItem(location.pathname);
@@ -39,7 +42,15 @@ const Header = () => {
                     name='Пользователь'
                 >
                     {profileData.name+" "+profileData.surname}
-                    <Button inverted color='violet' className='button-exit' size='mini' compact onClick={onLogout} icon='sign-out'/>
+                    <Button title='Выйти' inverted color='violet' className='button-exit' size='mini' compact onClick={onLogout} icon='sign-out'/>
+                </Menu.Item>}
+
+                {!isAuth && <Menu.Item
+                    position='right'
+                    name='Пользователь'
+                >
+                    Гость
+                    <Button title='Войти' inverted color='violet' className='button-exit' size='mini' compact onClick={()=>navigate(LOGIN_LINK)} icon='sign in'/>
                 </Menu.Item>}
             </Menu>
         </div>
