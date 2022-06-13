391d42e0d1dd19e270716e03321746f74865c5ef
mathyber <ilan23f@gmail.com>
2022-06-13 18:27:25 +0300

    ссылки на подачу заявки на главной странице

--- backend/controllers/contestController.js
+++ backend/controllers/contestController.js
@@ -1,12 +1,32 @@
 const contestData = require('../settings.json');
+const {Season} = require("../models/models");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 class ContestController {
     async contestData(req, res, next) {
         // #swagger.tags = ['Contest']
+
+        let date = Date.parse(new Date());
+
+        let seasons = await Season.findAll({
+            where: {
+                [Op.and]: [
+                    {dateEnd: {[Op.gt]: date}},
+                    {dateStart: {[Op.lt]: date}},
+                ],
+            },
+            order: [
+                ['id', 'DESC']
+            ]
+        });
+
+
         try {
             return res.json({
                 name: contestData.contestName,
                 description: contestData.description,
+                seasons
             });
         } catch (err) {
             next(err)
